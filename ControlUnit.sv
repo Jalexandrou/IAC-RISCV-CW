c7b9db83e032d73a34fe5a271441294ab4036710
James Donald <dev@chaoscantrip.com>
2022-11-30 22:15:34 +0000

    Changed ControlUnit IO to match target

--- ControlUnit.sv
+++ ControlUnit.sv
@@ -1,11 +1,16 @@
-module ControlUnit #()(
-	input 	logic               EQ,
-	input 	[6:0]   			instr,	
-	output 	logic               RegWrite,	// enable to write regs
-	output 	[2:0]               ALUctrl,
-	output 	logic               ALUsrc,		
-	output 	[2:0]               ImmSrc,		
-	output 	logic               PCsrc	
+module ControlUnit #(
+    parameter INSTR_WIDTH = 32
+)(
+	input 	logic               eq_i,
+	input   [INSTR_WIDTH-1:0]   instr_i,
+
+	output  logic               PCSrc_o,
+    output  logic               ResultSrc_o,
+    output  logic               MemWrite_o,
+    output  [2:0]               ALUControl_o,
+    output  logic               ALUSrc_o,
+    output  [1:0]               ImmSrc_o,
+    output  logic               RegWrite_o
 );
 
 typedef enum bit[6:0]   {addi = 7'b0010011, bne = 7'b1100011}   Opcode;
