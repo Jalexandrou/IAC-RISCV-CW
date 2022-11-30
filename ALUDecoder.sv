e94e8f9b559716640a8a046daa0b3164f578eddc
James Donald <dev@chaoscantrip.com>
2022-11-30 23:06:42 +0000

    Added combination logic for ALUDecoder

--- ALUDecoder.sv
+++ ALUDecoder.sv
@@ -7,4 +7,26 @@ module ALUDecoder #()(
     output [2:0]    ALUControl_o
 );
 
+always_comb begin
+    case(ALUOp)
+        2'b00: ALUControl_o = 3'b000;
+        2'b01: ALUControl_o = 3'b001;
+        2'b10: begin
+            case(func3)
+                3'b010: ALUControl_o = 3'b101;
+                3'b110: ALUControl_o = 3'b011;
+                3'b111: ALUControl_o = 3'b010;
+                3'b000: begin
+                    case({op5, func7[5]})
+                        2'b11: ALUControl_o = 3'b001;
+                        default: ALUControl_o = 3'b000;
+                    endcase
+                end
+                default: ALUControl_o = 3'b111; //Idk what to put as of yet
+            endcase
+        end
+        default: ALUControl_o = 3'b111; //Idk what to put as of yet
+    endcase
+end
+
 endmodule
