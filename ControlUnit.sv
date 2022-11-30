4c07d833a2d7ff16d2469b093a55e4e742f7cf20
James Donald <dev@chaoscantrip.com>
2022-11-30 22:54:18 +0000

    Moved opcode typedef into ControlUnitDecoder

--- ControlUnit.sv
+++ ControlUnit.sv
@@ -44,19 +44,6 @@ module ControlUnit #(
         .RegWrite_o     (RegWrite_o)
     )
 
-
-typedef enum bit[6:0]   {
-        RType    =   7'b0110011,
-        Load     =   7'b0000011,
-        IType    =   7'b0010011,
-        SType    =   7'b0100011,
-        BType    =   7'b1100011,
-        AddUpp   =   7'b0010111,
-        LoadUpp  =   7'b0110111,
-        JumpImm  =   7'b1100111,
-        JumpLink =   7'b1101111
-    }                           Opcode;
-
     always_comb begin
     	case (instr[6:0])
             addi: begin	
--- ControlUnitDecoder.sv
+++ ControlUnitDecoder.sv
@@ -9,4 +9,16 @@ module ControlUnitDecoder #()(
     output  logic               RegWrite_o
 );
 
+typedef enum bit[6:0]   {
+        RType    =   7'b0110011,
+        Load     =   7'b0000011,
+        IType    =   7'b0010011,
+        SType    =   7'b0100011,
+        BType    =   7'b1100011,
+        AddUpp   =   7'b0010111,
+        LoadUpp  =   7'b0110111,
+        JumpImm  =   7'b1100111,
+        JumpLink =   7'b1101111
+    }                           Opcode;
+
 endmodule
