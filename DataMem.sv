5067e58bca082de8b676e20e22844948a342f488
Jacob <59978422+JacobWhiteHat@users.noreply.github.com>
2022-12-02 10:58:40 +0000

    Fix

--- DataMem.sv
+++ DataMem.sv
@@ -9,7 +9,7 @@ module DataMem #(
     output logic [DATA_WIDTH-1:0]        readData;
 );
 
-    logic [DATA_WIDTH-1:0] ram_array [2**ADDRESS_WIDTH=1:0];
+    logic [DATA_WIDTH-1:0] ram_array [2**ADDRESS_WIDTH-1:0];
 
     always_ff @(posedge clk) begin
         if (we == 1'b1)
