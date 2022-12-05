64ae19cac8950fe4477218b24adfad8686dca602
Jacob <59978422+JacobWhiteHat@users.noreply.github.com>
2022-12-05 16:48:12 +0000

    Update f1.asm
    a0 increments from 0 -> 8

--- f1.asm
+++ f1.asm
@@ -1,4 +1,9 @@
+    addi t1, zero, 0x8      # load t1 with 8
+    addi a0, zero, 0x0      # a0 used for output
+mloop:
+    addi a1, zero, 0x0      # a1 is counter, init to 0
 iloop:
-    add a1, a1, a1
-    addi a1, a1, 1
-    add a0, a1, x0
+    addi a0, a1, 0          # load a0 with a1
+    addi a1, a1, 1          # increment a1
+    bne at, t1, iloop       # if a1 !=9, branch to i loop
+    bne t1, zero, mloop     # else always branch to mloop
