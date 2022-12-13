bed8897bd3a7b0a4d894cca0ce007a8e5ae4f5f0
Jalexandrou <jalexandrou10@gmail.com>
2022-12-13 23:23:26 +0000

    Corrected simple f1 program

--- src/myprog/f1.s
+++ src/myprog/f1.s
@@ -7,7 +7,7 @@ loop:
     jal     ra, shift          # execute shift subroutine
     j       loop               # loop forever
 init:
-    addi    t1, zero, 0x100    # load t1 with 256
+    addi    t1, zero, 0xFF     # load t1 with 255
     ret
 reset:
     addi    a0, zero, 0x0      # a0 used for output
@@ -16,6 +16,7 @@ reset:
 shift:
     addi    a0, a1, 0          # load a0 with a1 
     slli    a1, a1, 1          # shift a1 left by 1 bit
+    addi    a1, a1, 1          # increment a1 by 1
+    bne     a1, t1, shift      # if a1 !=255, branch to shift
     addi    a0, a1, 0          # load a0 with a1 
-    bne     a1, t1, shift      # if a1 !=256, branch to shift
     ret         
