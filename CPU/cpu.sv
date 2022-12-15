fd347553f32e1adce1195f24ddffdee5973009ce
derinak21 <116320304+derinak21@users.noreply.github.com>
2022-12-15 18:17:39 +0000

    Corrected syntax

--- CPU/cpu.sv
+++ CPU/cpu.sv
@@ -3,24 +3,21 @@ module cpu #(
 )(
     input logic                     clk,   // Input/Output Logic
     input logic                     rst,
-    output logic [DATA_WIDTH-1:0]   a0                  
+    output logic [DATA_WIDTH-1:0]   a0,   
+
 );
 
 //interconnecting wires before first pipeline register
-    logic [DATA_WIDTH-1:0] RD;          
+             
     logic [DATA_WIDTH-1:0] PCF;
     logic [DATA_WIDTH-1:0] next_pc;
-    logic [DATA_WIDTH-1:0] PC_Plus4F;
-    logic                  PCsrcRegE;
+    logic [DATA_WIDTH-1:0] PCPlus4F;
 
 //interconnecting wires before second pipeline register
-    logic [DATA_WIDTH-1:0] InstrD;
     logic [DATA_WIDTH-1:0] PCD;
-    logic [DATA_WIDTH-1:0] PC_Plus4D;        
+    logic [DATA_WIDTH-1:0] PCPlus4D;        
     logic                  RegWriteD;
-    logic [1:0]            ResultSrcD;
-    logic                  JumpD;
-    logic                  BranchD;
+    logic                  ResultSrcD;
     logic [DATA_WIDTH-1:0] ImmExtD;          
     logic [2:0]            ImmSrcD;
     logic                  MemWriteD;
@@ -29,52 +26,55 @@ module cpu #(
     logic                  PCsrcRegD;
     logic [DATA_WIDTH-1:0] RD1;        
     logic [DATA_WIDTH-1:0] RD2;
-    logic [DATA_WIDTH-1:0] RdD;
     logic                  branchD;
     logic                  JlinkD;
-    
+    logic                  StorePCD;
+    logic                  ByteOpD;
+    logic [DATA_WIDTH-1:0] RD;
+    logic [DATA_WIDTH-1:0] InstrD;
+
+
 //interconnecting wires before third pipeline register
     logic [DATA_WIDTH-1:0] PCE;
     logic [DATA_WIDTH-1:0] RD1E;         
     logic [DATA_WIDTH-1:0] RD2E;
-    logic [DATA_WIDTH-1:0] RdE;  
-    logic [DATA_WIDTH-1:0] ImmExtE;          
     logic [DATA_WIDTH-1:0] ALUResultE;
-    logic [DATA_WIDTH-1:0] PC_Plus4E;        
-    logic                  RegWriteE;
-    logic [1:0]            ResultSrcE;
-    logic                  JumpE;
-    logic                  BranchE;
     logic [DATA_WIDTH-1:0] ImmExtE;          
+    logic [DATA_WIDTH-1:0] PCPlus4E; 
+    logic [4:0]            RdE;       
+    logic                  RegWriteE;
+    logic                  ResultSrcE;
     logic                  MemWriteE;
     logic [2:0]            ALUControlE;
-    logic                  ALUsrcE;
+    logic                  ALUSrcE;
     logic                  ZeroE;
     logic                  PCsrcE;
     logic                  PCsrcRegE;
     logic                  StorePCE;
     logic                  branchE;
     logic                  JlinkE;
-    
+    logic                  ByteOpE;
+
 //interconnecting wires before fourth pipeline register
-    logic [DATA_WIDTH-1:0] PC_Plus4M;
-    logic [DATA_WIDTH-1:0] RdM;      
+    logic [DATA_WIDTH-1:0] PCPlus4M;
+    logic [4:0]            RdM;      
     logic [DATA_WIDTH-1:0] ALUResultM;
     logic [DATA_WIDTH-1:0] WriteDataM;
     logic [DATA_WIDTH-1:0] ReadDataM;     
     logic                  RegWriteM;
-    logic [1:0]            ResultSrcM;
+    logic                  ResultSrcM;
     logic                  MemWriteM;
     logic                  StorePCM;
+    logic                  ByteOpM;
 
 //interconnecting wires after fourth pipeline register
     logic                  RegWriteW;
-    logic [1:0]            ResultSrcW;
+    logic                  ResultSrcW;
     logic [DATA_WIDTH-1:0] ReadDataW;     
-    logic [DATA_WIDTH-1:0] RdW;      
-    logic [DATA_WIDTH-1:0] PC_Plus4W;
+    logic [4:0]            RdW;      
+    logic [DATA_WIDTH-1:0] PCPlus4W;
     logic                  StorePCW;
-
+    logic [DATA_WIDTH-1:0] ALUResultW;
 
     PC_Next PCMux (
         .PC_i (PCF),
@@ -84,7 +84,7 @@ module cpu #(
         .PCsrc_i (PCsrcE),
         .PCsrcReg_i (PCsrcRegE),
         .PC_Next_o (next_pc),
-        .PC_Plus4_o (PC_Plus4F)
+        .PC_Plus4_o (PCPlus4F)
     );
 
     PC_Register PCReg (
@@ -112,7 +112,7 @@ module cpu #(
         .ad2 (InstrD[24:20]),
         .ad3 (RdW),
         .we3 (RegWriteW),
-        .wd3 (StorePCW ? PC_Plus4 : (ResultSrcW ? ReadDataW : ALUResultM)), 
+        .wd3 (StorePCW ? PCPlus4W : (ResultSrcW ? ReadDataW : ALUResultW)), 
         .rd1 (RD1),
         .rd2 (RD2),
         .a0 (a0)
@@ -126,7 +126,6 @@ module cpu #(
 
     ControlUnit ControlUnit (
         .instr_i (InstrD),
-        .zero_i  (ZeroE),
         .PCSrcReg_o (PCsrcRegD),
         .StorePC_o  (StorePCD), 
         .ResultSrc_o (ResultSrcD),
@@ -135,7 +134,7 @@ module cpu #(
         .ALUSrc_o (ALUSrcD),
         .ImmSrc_o (ImmSrcD),
         .RegWrite_o (RegWriteD),
-        .branch_o(BranchD),
+        .branch_o(branchD),
         .Jlink_o(JlinkD),
         .ByteOp(ByteOpD)
     );
@@ -147,7 +146,7 @@ module cpu #(
         RD1E <= RD1;
         RD2E <= RD2;
         PCE <= PCD;
-        RdE <= RdD;
+        RdE <= InstrD[11:7];
         ImmExtE <= ImmExtD;
         PCPlus4E <= PCPlus4D;
         RegWriteE <= RegWriteD;
@@ -155,13 +154,14 @@ module cpu #(
         MemWriteE <= MemWriteD;
         ALUControlE <= ALUControlD;
         ALUSrcE <= ALUSrcD;
-        ImmSrcE <= ImmSrcD;
         StorePCE <= StorePCD;
         branchE <= branchD;
         JlinkE <= JlinkD;
         ByteOpE <= ByteOpD;
     end 
     
+    assign PCsrcE= branchE && !ZeroE | JlinkE;
+
     ALU ALU (
         .ALUop1 (RD1E),
         .ALUop2 (ALUSrcE ? ImmExtE : RD2E),
@@ -173,7 +173,7 @@ module cpu #(
     always_ff @ (negedge clk) begin
         //register after ALU
         ALUResultM <= ALUResultE;
-        WriteDataM <= WriteDataE;
+        WriteDataM <= RD2E;
         RdM <= RdE;
         PCPlus4M <= PCPlus4E;
         RegWriteM <= RegWriteE;
@@ -194,12 +194,13 @@ module cpu #(
     
     always_ff @ (negedge clk) begin 
         //register after data memory
-        ReadDataW <= Rd;
+        ReadDataW <= ReadDataM;
         RdW <= RdM;
         PCPlus4W <= PCPlus4M;
         RegWriteW <= RegWriteM;
         ResultSrcW <= ResultSrcM;
         StorePCW <= StorePCM;
+        ALUResultW <= ALUResultM;
     end 
 
 endmodule
