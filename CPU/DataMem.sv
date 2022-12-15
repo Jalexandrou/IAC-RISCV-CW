85fc204e7c8211996420d0c7bd5fe5379ac01565
Jacob <59978422+JacobWhiteHat@users.noreply.github.com>
2022-12-15 19:53:06 +0000

    Updated Parameters

--- CPU/DataMem.sv
+++ CPU/DataMem.sv
@@ -1,8 +1,10 @@
 module DataMem #(
     parameter   ADDRESS_WIDTH = 32,
                 DATA_WIDTH = 32,
-                BYTE_WIDTH = 8
-                CACHE_SIZE = 64
+                BYTE_WIDTH = 8,
+                SET_WIDTH = 8,
+                CACHE_WIDTH = DATA_WIDTH + (ADDRESS_WIDTH - SET_WIDTH - 2) + 1   // Data + Tag + V   
+                
 )(
     input  logic                         clk,
     input  logic                         we,
@@ -12,8 +14,8 @@ module DataMem #(
     output logic [DATA_WIDTH-1:0]        ReadData
 );
 
-    logic [BYTE_WIDTH-1:0] ram_array [32'h0001FFFF:32'h00000000];   // set mem size
-    logic [DATA_WIDTH-1:0] cache_array [CACHE_SIZE-1:0][2:0];
+    logic [BYTE_WIDTH-1:0] ram_array [32'h0001FFFF:32'h00000000]; 
+    logic [CACHE_WIDTH-1:0] cache_array [2**SET_WIDTH-1:0][2:0];
 
     initial begin
         $readmemh("gaussian.mem", ram_array, 32'h10000);
@@ -21,7 +23,7 @@ module DataMem #(
 
     always_ff @(posedge clk) begin
         if (we && !ByteOp) begin                 
-            ram_array[{Address[31:2], 2'b0}]   <= WriteData[31:24];       // Big endian storage  
+            ram_array[{Address[31:2], 2'b0}]   <= WriteData[31:24];
             ram_array[{Address[31:2], 2'b0}+1] <= WriteData[23:16];
             ram_array[{Address[31:2], 2'b0}+2] <= WriteData[15:8];
             ram_array[{Address[31:2], 2'b0}+3] <= WriteData[7:0];
