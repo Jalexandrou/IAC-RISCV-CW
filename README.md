e9ac7cad9be4b948174814bc9e01bce3e1e4cb5b
Jalexandrou <jalexandrou10@gmail.com>
2022-12-16 23:48:06 +0000

    Added rtl and test folders

--- README.md
+++ README.md
@@ -2,32 +2,3 @@
 
 > [Project Brief Repo](https://github.com/EIE2-IAC-Labs/Project_Brief)
 
-## Table for Control Unit Signals
-
-| Opcode | Branch_o | ResultSrc_o | MemWrite_o | ALUSrc_o | ImmSrc_o | RegWrite_o |
-| --- | --- | --- | --- | --- | --- | --- |
-| R-Type | 0 | 0 | 0 | 0 | xx | 1 |
-| Load Mem | 0 | 1 | 0 | 1 | 000 | 1 |
-| I-Type | 0 | 0 | 0 | 1 | 000 | 1 |
-| Store Mem | 0 | x | 1 | 1 | 001 | 0 |
-| Branch | 1 | x | 0 | 0 | 010 | 0 |
-| rd = uppimm + PC | ? | ? | ? | ? | 100 | ? |
-| rd = uppimm | ? | ? | ? | ? | 100 | ? |
-| Jump and Link Reg | ? | ? | ? | ? | 011 | ? |
-| Jump and Link | ? | ? | ? | ? | 011 | ? |
-| default? | ? | ? | ? | ? | ? | ? |
-
-| Signal | 0 | 1 |
-| --- | --- | --- |
-| Branch_o | Adds with 0 so PCSrc == 1? PCNext increments by SignExtend | PCNext increments by 4 |
-| ResultSrc_o | Register saves data from ALUResult | Register saves data from Memory->ReadData |
-| MemWrite_o | Memory does not write | Memory saves WD in A |
-| ALUSrc_o | ALU->SrcB reads from Reg->RD2 | ALU->SrcB reads from SignExtend |
-| RegWrite_o | Register does not write | Register saves WD in A |
-
-| Signal | I-Type | Store | Branch | Jump | UpImm |
-| --- | --- | --- | --- | --- | --- |
-| ImmSrc_o | 000 | 001 | 010 | 011 | 100 |
-
-![image](https://user-images.githubusercontent.com/59978422/205101131-365f9510-62d7-4854-b699-884c128b761f.png)
-
---
+++ rtl/cpu.sv
@@ -0,0 +1,204 @@
+module cpu #(
+    parameter   DATA_WIDTH = 32            // Parameters
+)(
+    input logic                     clk,   // Input/Output Logic
+    input logic                     rst,
+    output logic [DATA_WIDTH-1:0]   a0
+);
+
+//interconnecting wires before first pipeline register
+             
+    logic [DATA_WIDTH-1:0] PCF;
+    logic [DATA_WIDTH-1:0] next_pc;
+    logic [DATA_WIDTH-1:0] PCPlus4F;
+
+//interconnecting wires before second pipeline register
+    logic [DATA_WIDTH-1:0] PCD;
+    logic [DATA_WIDTH-1:0] PCPlus4D;        
+    logic                  RegWriteD;
+    logic                  ResultSrcD;
+    logic [DATA_WIDTH-1:0] ImmExtD;          
+    logic [2:0]            ImmSrcD;
+    logic                  MemWriteD;
+    logic [2:0]            ALUControlD;
+    logic                  ALUSrcD;
+    logic                  PCsrcRegD;
+    logic [DATA_WIDTH-1:0] RD1;        
+    logic [DATA_WIDTH-1:0] RD2;
+    logic                  branchD;
+    logic                  JlinkD;
+    logic                  StorePCD;
+    logic                  ByteOpD;
+    logic [DATA_WIDTH-1:0] RD;
+    logic [DATA_WIDTH-1:0] InstrD;
+
+
+//interconnecting wires before third pipeline register
+    logic [DATA_WIDTH-1:0] PCE;
+    logic [DATA_WIDTH-1:0] RD1E;         
+    logic [DATA_WIDTH-1:0] RD2E;
+    logic [DATA_WIDTH-1:0] ALUResultE;
+    logic [DATA_WIDTH-1:0] ImmExtE;          
+    logic [DATA_WIDTH-1:0] PCPlus4E; 
+    logic [4:0]            RdE;       
+    logic                  RegWriteE;
+    logic                  ResultSrcE;
+    logic                  MemWriteE;
+    logic [2:0]            ALUControlE;
+    logic                  ALUSrcE;
+    logic                  ZeroE;
+    logic                  PCsrcE;
+    logic                  PCsrcRegE;
+    logic                  StorePCE;
+    logic                  branchE;
+    logic                  JlinkE;
+    logic                  ByteOpE;
+
+//interconnecting wires before fourth pipeline register
+    logic [DATA_WIDTH-1:0] PCPlus4M;
+    logic [4:0]            RdM;      
+    logic [DATA_WIDTH-1:0] ALUResultM;
+    logic [DATA_WIDTH-1:0] WriteDataM;
+    logic [DATA_WIDTH-1:0] ReadDataM;     
+    logic                  RegWriteM;
+    logic                  ResultSrcM;
+    logic                  MemWriteM;
+    logic                  StorePCM;
+    logic                  ByteOpM;
+
+//interconnecting wires after fourth pipeline register
+    logic                  RegWriteW;
+    logic                  ResultSrcW;
+    logic [DATA_WIDTH-1:0] ReadDataW;     
+    logic [4:0]            RdW;      
+    logic [DATA_WIDTH-1:0] PCPlus4W;
+    logic                  StorePCW;
+    logic [DATA_WIDTH-1:0] ALUResultW;
+
+    PC_Next PCMux (
+        .PC_i (PCF),
+        .PC_i2 (PCE),
+        .ImmOp_i (ImmExtE),
+        .PC_Jalr_i (ALUResultE),
+        .PCsrc_i (PCsrcE),
+        .PCsrcReg_i (PCsrcRegE),
+        .PC_Next_o (next_pc),
+        .PC_Plus4_o (PCPlus4F)
+    );
+
+    PC_Register PCReg (
+        .PC_o         (PCF),
+        .PC_Next_i    (next_pc),
+        .clk          (clk),
+        .rst          (rst)
+    );
+    
+    InstrMem InstrMem (
+        .instr  (RD),
+        .PC     (PCF)
+    );
+
+    always_ff @ (negedge clk) begin
+    //register after instruction memory
+        InstrD <= RD;
+        PCD <= PCF;
+        PCPlus4D <= PCPlus4F;
+    end 
+    
+    RegFile RegFile (          
+        .clk (clk),
+        .ad1 (InstrD[19:15]),
+        .ad2 (InstrD[24:20]),
+        .ad3 (RdW),
+        .we3 (RegWriteW),
+        .wd3 (StorePCW ? PCPlus4W : (ResultSrcW ? ReadDataW : ALUResultW)), 
+        .rd1 (RD1),
+        .rd2 (RD2),
+        .a0 (a0)
+    );
+
+    SignExtend SignExtend (
+        .ImmOp (ImmExtD),
+        .ImmSrc (ImmSrcD),
+        .instr (InstrD[31:7])
+    );
+
+    ControlUnit ControlUnit (
+        .instr_i (InstrD),
+        .PCSrcReg_o (PCsrcRegD),
+        .StorePC_o  (StorePCD), 
+        .ResultSrc_o (ResultSrcD),
+        .MemWrite_o (MemWriteD),
+        .ALUControl_o (ALUControlD),
+        .ALUSrc_o (ALUSrcD),
+        .ImmSrc_o (ImmSrcD),
+        .RegWrite_o (RegWriteD),
+        .branch_o(branchD),
+        .Jlink_o(JlinkD),
+        .ByteOp(ByteOpD)
+    );
+
+    always_ff @ (negedge clk) begin
+        // register after control unit and register file
+        PCsrcRegE <= PCsrcRegD;
+        RD1E <= RD1;
+        RD2E <= RD2;
+        PCE <= PCD;
+        RdE <= InstrD[11:7];
+        ImmExtE <= ImmExtD;
+        PCPlus4E <= PCPlus4D;
+        RegWriteE <= RegWriteD;
+        ResultSrcE <= ResultSrcD;
+        MemWriteE <= MemWriteD;
+        ALUControlE <= ALUControlD;
+        ALUSrcE <= ALUSrcD;
+        StorePCE <= StorePCD;
+        branchE <= branchD;
+        JlinkE <= JlinkD;
+        ByteOpE <= ByteOpD;
+    end 
+
+    ALU ALU (
+        .ALUop1 (RD1E),
+        .ALUop2 (ALUSrcE ? ImmExtE : RD2E),
+        .ALUout (ALUResultE),
+        .zero_o (ZeroE),
+        .ALUctrl (ALUControlE)
+    );
+
+    assign PCsrcE = (branchE && !ZeroE) || JlinkE;
+    
+    always_ff @ (negedge clk) begin
+        //register after ALU
+        ALUResultM <= ALUResultE;
+        WriteDataM <= RD2E;
+        RdM <= RdE;
+        PCPlus4M <= PCPlus4E;
+        RegWriteM <= RegWriteE;
+        ResultSrcM <= ResultSrcE;
+        MemWriteM <= MemWriteE;
+        StorePCM <= StorePCE;
+        ByteOpM <= ByteOpE;
+    end 
+    
+    DataMem DataMem (
+        .clk (clk),
+        .Address (ALUResultM),
+        .WriteData (WriteDataM),
+        .we (MemWriteM),  
+        .ByteOp  (ByteOpM),
+        .ReadData (ReadDataM)
+    );
+    
+    always_ff @ (negedge clk) begin 
+        //register after data memory
+        ReadDataW <= ReadDataM;
+        RdW <= RdM;
+        PCPlus4W <= PCPlus4M;
+        RegWriteW <= RegWriteM;
+        ResultSrcW <= ResultSrcM;
+        StorePCW <= StorePCM;
+        ALUResultW <= ALUResultM;
+    end 
+
+endmodule
